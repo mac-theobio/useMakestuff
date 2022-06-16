ab065ac6a4dac4d70ce02ed9ac70df95bee0ab29
Jonathan Dushoff <dushoff@mcmaster.ca>
2022-06-16 13:33:08 -0400

    trying to clean

--- make_intro.mk
+++ make_intro.mk
@@ -21,6 +21,7 @@ README.md.words: README.md
 ## explicit rules always override implicit rules
 
 ## One more automatic variable is $*, which corresponds in recipe to the % in dependency
+Ignore += *.md.words
 %.md.words: %.md
 	@echo $* is my md file
 	wc < $< > $@

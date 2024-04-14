f6f1c7b79b56af3b66d7857f7bcc587317e35045
Jonathan Dushoff <dushoff@mcmaster.ca>
2024-04-14 19:26:41 -0400

    leaving attic

--- pipeRex.mk
+++ pipeRex.mk
@@ -1,3 +1,4 @@
+## This is to help makestuff/git.mk keep track of the repo; it can be ignored or deleted if you're not using that
 Sources += $(wildcard *.R)
 
 ## when autopipeR is set, make thinks it can make <x.Rout> from <x.R> using the recipe $(pipeR); we can also add that recipe manually.
@@ -14,7 +15,8 @@ calc.Rout: calc.R base.rda
 #### run base.R to make base.Rout (and base.rda as a side effect);
 #### then run calc.R, passing the name of the dependency for shellpipes to use.
 
-## To make a plot from a pipeR script, you should just need to plot
+## To make a plot from a pipeR script, you just need to 
+## print a plot (e.g., using ggplot() or plot()
 ## output will be automatically put into .Rout.pdf
 ## it's good to add startGraphics() after you call shellpipes, but simple projects always work even without that
 
